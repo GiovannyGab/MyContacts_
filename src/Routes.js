aa2fbd95d630792547ebe040b8717a096c509a91
Giovanny Gabriel <giovanny.gabrieldsn@gmail.com>
2023-05-16 18:08:45 -0300

    ultilizando da api e ordenando a lista por orderBy

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,10 @@
-import { Switch, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Edit from './pages/Edit';
 import NewContact from './pages/NewContact';
 
 function Routes() {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log('A URL mudou para:', location.pathname);
-  }, [location.pathname]);
   return (
     <Switch>
       <Route path="/" exact component={Home} />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,53 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Container, Header, ListContainer, ListBody, Card, InputSeachContainer,
+  Container,
+  Header,
+  ListContainer,
+  ListBody,
+  Card,
+  InputSeachContainer,
 } from './styles';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import deleteb from '../../assets/images/icons/deleteb.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [order, setOrder] = useState('asc');
+  useEffect(() => {
+    fetch(`http://localhost:3001/contacts?orderBy=${order}`)
+      .then(async (res) => {
+        const json = await res.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }, [order]);
+
+  function HandleToggleOrderBy() {
+    setOrder((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
+
   return (
     <Container>
-
       <InputSeachContainer>
         <input type="text" placeholder="Digite oque nome que quer procurar" />
       </InputSeachContainer>
       <Header>
-        <strong>Contatos 3</strong>
+        <strong>
+          {contacts.length}
+          {contacts.length === 1 ? ' Contato' : ' Contatos'}
+        </strong>
         <Link to="/new">Novo Contato</Link>
       </Header>
 
       <ListContainer>
         <header>
-          <button type="button" className="sort-button">
+          <button type="button" className="sort-button" onClick={HandleToggleOrderBy}>
             <span>Nome</span>
             <img src={arrow} alt="Arrow" />
           </button>
         </header>
         <ListBody>
-          <Card>
-            <div className="info">
-              <div className="contact-header">
-                <strong>Giovanny Gabriel</strong>
-                <small>Instagram</small>
-              </div>
-              <div className="contact-info">
-                <span>giovanny.gabrieldsn@gmail.com</span>
-                <span>87988261615</span>
-              </div>
-            </div>
+          {contacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contact-header">
+                  <strong>
+                    {contact.name}
+                  </strong>
+                  {contact.category_name && (
+                  <small>
+                    {contact.category_name}
+                  </small>
+                  )}
 
-            <div className="actions">
-              <Link to="/edit/123">
-                <img src={edit} alt="edit" />
-              </Link>
-              <button type="button">
-                <img src={deleteb} alt="delete" />
-              </button>
-            </div>
-          </Card>
+                </div>
+                <div className="contact-info">
+                  <span>{contact.email}</span>
+                  <span>{contact.phone}</span>
+                </div>
+              </div>
+
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="edit" />
+                </Link>
+                <button type="button">
+                  <img src={deleteb} alt="delete" />
+                </button>
+              </div>
+            </Card>
+          ))}
 
         </ListBody>
       </ListContainer>
