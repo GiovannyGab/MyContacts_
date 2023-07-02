704ce285bff9b99d5dc6b5535dadaff75ef9ca38
Giovanny Gabriel <giovanny.gabrieldsn@gmail.com>
2023-07-01 23:48:20 -0300

    refatorando o ref e retirando a dependencia do mesmo do componente

--- src/hooks/useAnimatedList.js
+++ src/hooks/useAnimatedList.js
@@ -1,33 +1,53 @@
 import {
-  createRef, useRef, useCallback, useState,
+  createRef, useRef, useCallback, useState, useEffect,
 } from 'react';
 
 export default function useAnimatedList() {
   const [pendingRemoveItemIds, setPendingRemoveItemIds] = useState([]);
   const [items, setItems] = useState([]);
   const animatedRefs = useRef(new Map());
-  const handleRemoveMessage = useCallback((id) => {
-    setPendingRemoveItemIds((prevState) => [...prevState, id]);
-  }, []);
+  const animationHasListener = useRef(new Map());
   const handleAnimationEnd = useCallback((id) => {
     setItems((prevState) => prevState.filter((item) => item.id !== id));
     setPendingRemoveItemIds(
       (prevState) => prevState.filter((itemID) => itemID !== id),
     );
   }, []);
+
+  useEffect(() => {
+    pendingRemoveItemIds.forEach((itemID) => {
+      const animatedRef = animatedRefs.current.get(itemID);
+      const animatedElement = animatedRef?.current;
+      const alredyHasListener = animationHasListener.current.has(itemID);
+      if (animatedElement && !alredyHasListener) {
+        const onAnimationEnd = () => { handleAnimationEnd(itemID); };
+        animationHasListener.current.set(itemID, true);
+        animatedElement.addEventListener(
+          'animationend',
+          onAnimationEnd,
+        );
+      }
+    });
+  }, [pendingRemoveItemIds]);
+
+  const handleRemoveMessage = useCallback((id) => {
+    setPendingRemoveItemIds((prevState) => [...prevState, id]);
+  }, []);
+
   const getAnimatedRef = useCallback((itemID) => {
     let animatedRef = animatedRefs.current.get(itemID);
     if (!animatedRef) {
       animatedRef = createRef();
       animatedRefs.current.set(itemID, animatedRef);
     }
+    return animatedRef;
   }, []);
   // eslint-disable-next-line max-len
   const renderList = useCallback(
     (renderItem) => items.map((item) => {
       const isLeaving = pendingRemoveItemIds.includes(item.id);
       const animatedRef = getAnimatedRef(item.id);
-      console.log({ animatedRef });
+
       return renderItem(item, { isLeaving, animatedRef });
     }),
     [items, pendingRemoveItemIds],
